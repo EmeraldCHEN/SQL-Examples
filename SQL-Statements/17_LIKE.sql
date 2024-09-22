9a96aec3bda7d5dbcdcfc17af7ec62caf40b2fac
EC <ahyuancy@gmail.com>
2024-09-23 09:42:48 +1200

    Add more sql examples

--- SQL-Statements/17_LIKE.sql
+++ SQL-Statements/17_LIKE.sql
@@ -1,20 +1,74 @@
 /*  The LIKE operator is used in a WHERE clause to search for a specified pattern in a column  */
 
 
-/***  Example: Select  table  ***/
+/***  Example: Select all customers that starts with the letter "a" ***/
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a%'; 
 
 
+/***  Example: Select all customers that does not start with "a" ***/
 
-/***  Example:  ***/
-
-
-/***  Example:  ***/
-
-/***  Example:  ***/
+SELECT * FROM Customers
+WHERE CustomerName NOT LIKE 'a%'; 
 
 
-/***  Example:  ***/
+/***  Example: Return all customers from a city that starts with 'L' followed by one wildcard character, then 'nd' and then two wildcard characters ***/
+
+SELECT * FROM Customers
+WHERE city LIKE 'L_nd__'; 
 
 
-/***  Example:  ***/
+/***  Example: Return all customers from a city that contains the letter 'L' ***/
+
+SELECT * FROM Customers
+WHERE city LIKE '%L%'; 
+
+
+/***  Example: Return all customers that starts with 'La' ***/
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'La%'; 
+
+
+/***  Example: Return all customers that starts with 'a' or starts with 'b' ***/
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a%' OR CustomerName LIKE 'b%';
+
+
+/***  Example: Return all customers that ends with 'a' ***/
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%a'; 
+
+
+/***  Example: Return all customers that starts with "b" and ends with "s" ***/
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'b%s'; 
+
+
+/***  Example: Return all customers that contains the phrase 'or' ***/
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '%or%';
+
+
+/***  Example: Return all customers that starts with "a" and are at least 3 characters in length ***/
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE 'a__%'; 
+
+
+/***  Example: Return all customers that have "r" in the second position ***/
+
+SELECT * FROM Customers
+WHERE CustomerName LIKE '_r%';
+
+
+/***  Example: Return all customers from Spain ***/
+
+SELECT * FROM Customers
+WHERE Country LIKE 'Spain';
 
--- SQL-Statements/18_.sql
+++ SQL-Statements/18_.sql
@@ -8,18 +8,6 @@
 
 
 
+/***  Example:  ***/
 
-Are you preparing for a Manual Testing interview? Here are some key questions to help you brush up on your knowledge:
-
-- Explain the process of agile project.
-- What all ceremonies are there in agile?
-- Explain Sprint planning and Retrospective meeting.
-- How to prioritise the user stories when running out out of time?
-- Difference between retesting and regression testing?
-- Explain defect life cycle.
-- Difference between deferred and rejected bug.
-- Difference between test plan and strategy.
-- What are the key challenges of software testing?
-- How do system testing and integration testing differ?
-- When do you decide to stop testing?
-- How do you handle testing for a product with frozen requirements?
+/***  Example:  ***/
