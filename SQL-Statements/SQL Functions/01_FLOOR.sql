929980a9ae9b182e9857b502df9ded9198d64af0
EC <ahyuancy@gmail.com>
2024-10-12 11:56:37 +1300

    Add new SQL query

---
+++ SQL-Statements/SQL Functions/01_FLOOR.sql
@@ -0,0 +1,12 @@
+/*  The FLOOR() function returns the largest integer value that is smaller than or equal to a number  */
+
+
+/***  Example: Return the largest integer value that is smaller than or equal to the number  ***/
+
+SELECT FLOOR(29.75) AS FloorValue; -- 29
+
+SELECT FLOOR(29) AS FloorValue; -- 29
+
+SELECT FLOOR(-19.5) AS FloorValue; -- -20
+
+
