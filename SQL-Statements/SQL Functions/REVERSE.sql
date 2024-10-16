67ee5c9163afb0a26d557b0ef6dda1c7c6d4c27d
EC <ahyuancy@gmail.com>
2024-10-16 20:06:24 +1300

    Add new SQL query

---
+++ SQL-Statements/SQL Functions/REVERSE.sql
@@ -0,0 +1,14 @@
+/*  The REVERSE() function reverses a string and returns the result  */
+
+
+/***  Example ***/
+
+SELECT REVERSE('SQL Tutorial');  -- lairotuT LQS
+
+
+SELECT REVERSE(CustomerName)
+FROM Customers;
+
+
+
+
