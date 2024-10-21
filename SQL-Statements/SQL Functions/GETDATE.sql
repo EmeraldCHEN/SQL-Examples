6e403caf538f8f09e83db52e48d7b24245050ff4
EC <ahyuancy@gmail.com>
2024-10-21 21:04:32 +1300

    Add new SQL query

---
+++ SQL-Statements/SQL Functions/GETDATE.sql
@@ -0,0 +1,18 @@
+/*  
+The GETDATE() function returns the current database system date and time, in a 'YYYY-MM-DD hh:mm:ss.mmm' format  
+
+The GETUTCDATE() function returns the current database system UTC date and time, in a 'YYYY-MM-DD hh:mm:ss.mmm' format
+*/
+
+
+/***  Example: Return the current database system date and time  ***/
+
+SELECT GETDATE(); 
+
+
+/***  Example: Return the current UTC date and time  ***/
+
+SELECT GETUTCDATE(); 
+
+
+
